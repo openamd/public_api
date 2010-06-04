432816788bc25b12b9654309c62516516939ddcb
Jonathan Dahan <jedahan@gmail.com>
2010-06-04 02:13:49 +0000

    testing new routes

--- public_api_site/urls.py
+++ public_api_site/urls.py
@@ -5,13 +5,18 @@ from django.conf.urls.defaults import *
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    # TODO: remove trailing slashes
     (r'^locations/$', 'public_api_site.api.locations.index'),
+    # NOTE: current testing
+    (r'^testing/$',  'public_api_site.api.views.view'),
+
     (r'^speakers/$',  'public_api_site.api.views.speakers'),
     (r'^talks/$',     'public_api_site.api.talks.index'),
     (r'^interests/$', 'public_api_site.api.interests.index'),
     (r'^stats/$',     'public_api_site.api.stats.index'),
 
 
+    # FIXME: we can probably ignore this
     (r'^speaker(?P<name>[^/]+)$', 'api.speakers.view'),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
